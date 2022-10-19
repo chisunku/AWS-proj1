f8ea9b42e7be502678673e64c8a2fd4910f3dd1d
EC2 Default User <ec2-user@ip-172-31-21-108.us-east-2.compute.internal>
2022-10-19 06:19:46 +0000

    commiting all files

---
+++ build/precache-manifest.1295358b1b1f5faeb8bf0ea69571e29b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0df4374159d3c251bf7a4283866def48",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3e814e597368c099e192",
+    "url": "/static/css/main.183a5e9d.chunk.css"
+  },
+  {
+    "revision": "063d297c0622a12b9a8e",
+    "url": "/static/js/2.1b9980dc.chunk.js"
+  },
+  {
+    "revision": "3e814e597368c099e192",
+    "url": "/static/js/main.9e4a21cc.chunk.js"
+  },
+  {
+    "revision": "9f3222c94b4bedd2ede2",
+    "url": "/static/js/runtime-main.7027a17f.js"
+  }
+]);
